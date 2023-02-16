7741e6b2536d87177a83f2daca320aff843c005f
Yuan <lugir.com@gmail.com>
2023-02-16 21:55:09 +0800

    Add test for cacheability headers.

--- tests/src/Functional/MixDevelopmentModeTest.php
+++ tests/src/Functional/MixDevelopmentModeTest.php
@@ -53,6 +53,10 @@ class MixDevelopmentModeTest extends BrowserTestBase {
       ->set('error_level', ERROR_REPORTING_HIDE)
       ->save();
 
+    // Disable cacheablility header debug.
+    $this->setContainerParameter('http.response.debug_cacheability_headers', FALSE);
+    $this->rebuildContainer();
+    $this->resetAll();
   }
 
   /**
@@ -123,14 +127,16 @@ class MixDevelopmentModeTest extends BrowserTestBase {
     $this->assertSession()->elementNotExists('xpath', '//link[contains(@href, "/core/modules/system/css/components/js.module.css")]');
     // No dev mode message.
     $this->assertSession()->linkNotExists('Go online.');
-
+    // No cacheability headers.
+    $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Cache-Contexts');
+    $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Cache-Tags');
+    $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Cache-Max-Age');
     // Second visit.
     $this->drupalGet($this->testPath);
     // Page cache hit.
     $this->assertSession()->responseHeaderContains('X-Drupal-Cache', 'Hit');
     // No dynamic page cache for anonymous user.
     $this->assertSession()->responseHeaderContains('X-Drupal-Dynamic-Cache', 'Miss');
-
     // No error message.
     $this->drupalGet($this->errorTestPath);
     $this->assertSession()->statusCodeEquals(200);
@@ -149,17 +155,19 @@ class MixDevelopmentModeTest extends BrowserTestBase {
     $this->assertFalse($this->container->get('twig')->isAutoReload());
     // No twig debug info.
     $this->assertStringNotContainsString('<!-- THEME DEBUG -->', $pageHtml, 'Twig debug markup not found in page source code when development mode is not enabled.');
-    $assertSession = $this->assertSession();
     // No page cache header for authenticated user.
-    $assertSession->responseHeaderDoesNotExist('X-Drupal-Cache');
+    $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Cache');
     // Dynamic page cache MISS on first visit.
-    $assertSession->responseHeaderContains('X-Drupal-Dynamic-Cache', 'Miss');
+    $this->assertSession()->responseHeaderContains('X-Drupal-Dynamic-Cache', 'Miss');
     // CSS/JS aggregated.
-    $assertSession->elementNotExists('xpath', '//script[contains(@src, "/core/misc/drupal.js")]');
-    $assertSession->elementNotExists('xpath', '//link[contains(@href, "/core/modules/system/css/components/js.module.css")]');
+    $this->assertSession()->elementNotExists('xpath', '//script[contains(@src, "/core/misc/drupal.js")]');
+    $this->assertSession()->elementNotExists('xpath', '//link[contains(@href, "/core/modules/system/css/components/js.module.css")]');
     // No dev mode message.
-    $assertSession->linkNotExists('Go online.');
-
+    $this->assertSession()->linkNotExists('Go online.');
+    // No cacheability headers.
+    $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Cache-Contexts');
+    $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Cache-Tags');
+    $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Cache-Max-Age');
     // Second visit.
     $this->drupalGet($this->testPath);
     $assertSession = $this->assertSession();
@@ -167,7 +175,6 @@ class MixDevelopmentModeTest extends BrowserTestBase {
     $assertSession->responseHeaderDoesNotExist('X-Drupal-Cache');
     // Dynamic page cache HIT on second visit.
     $assertSession->responseHeaderContains('X-Drupal-Dynamic-Cache', 'Hit');
-
     // No error message.
     $this->drupalGet($this->errorTestPath);
     $this->assertSession()->statusCodeEquals(200);
@@ -185,19 +192,20 @@ class MixDevelopmentModeTest extends BrowserTestBase {
     $this->assertTrue($this->container->get('twig')->isAutoReload());
     // Twig debug enabled.
     $this->assertStringContainsString('<!-- THEME DEBUG -->', $pageHtml, 'Twig debug markup found in page source code when development mode is enabled.');
-
-    $assertSession = $this->assertSession();
     // No dynamic page cache.
-    $assertSession->responseHeaderDoesNotExist('X-Drupal-Dynamic-Cache');
+    $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Dynamic-Cache');
     // No page cache.
-    $assertSession->responseHeaderDoesNotExist('X-Drupal-Cache');
-    $assertSession->responseHeaderContains('Cache-Control', 'must-revalidate, no-cache, private');
-
+    $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Cache');
+    $this->assertSession()->responseHeaderContains('Cache-Control', 'must-revalidate, no-cache, private');
     // CSS/JS not aggregated.
-    $assertSession->elementExists('xpath', '//script[contains(@src, "/core/misc/drupal.js")]');
-    $assertSession->elementExists('xpath', '//link[contains(@href, "/core/modules/system/css/components/js.module.css")]');
+    $this->assertSession()->elementExists('xpath', '//script[contains(@src, "/core/misc/drupal.js")]');
+    $this->assertSession()->elementExists('xpath', '//link[contains(@href, "/core/modules/system/css/components/js.module.css")]');
     // Dev mode message.
-    $assertSession->linkExists('Go online.');
+    $this->assertSession()->linkExists('Go online.');
+    // Cacheability headers exist.
+    $this->assertSession()->responseHeaderExists('X-Drupal-Cache-Contexts');
+    $this->assertSession()->responseHeaderExists('X-Drupal-Cache-Tags');
+    $this->assertSession()->responseHeaderExists('X-Drupal-Cache-Max-Age');
 
     // Second visit.
     $pageHtml = $this->drupalGet($this->testPath);
@@ -206,12 +214,10 @@ class MixDevelopmentModeTest extends BrowserTestBase {
     // CSS/JS still not aggregated.
     $this->assertStringContainsString('/core/modules/system/css/components/js.module.css', $pageHtml, 'js.module.css not found in page source code when development mode is enabled.');
     $this->assertStringContainsString('/core/misc/drupal.js', $pageHtml, 'drupal.js not found in page source code when development mode is enabled.');
-
     // Still no dynamic page cache.
     $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Dynamic-Cache');
     // Still no page cache.
     $this->assertSession()->responseHeaderDoesNotExist('X-Drupal-Cache');
-
     // Error message shows.
     $this->drupalGet($this->errorTestPath);
     $this->assertSession()->statusCodeEquals(200);
